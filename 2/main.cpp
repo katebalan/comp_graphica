efb85135ea44ea6c04125cdc2b8c447e59382336
Kate Balan <ket11141@gmail.com>
2017-11-13 14:22:24 +0200

    save file

--- 2/main.cpp
+++ 2/main.cpp
@@ -2,25 +2,22 @@
 #include <GL/glut.h>   //Подключение библиотеки glut.h
 #include <cmath>
 #include <stdlib.h>
+#include <vector>
 
 #define _USE_MATH_DEFINES
 
 using namespace std;
 
-// double x = 0, y = 0, z = 80;
-// double lx = 0, ly = 0, lz = 0;
-// double angle_XY = 0, angle_YZ = 0;
-
 int x_camera = 0, y_camera = -20, z_camera = 50;
 double x_light = 0, y_light = 20, z_light = 4.0;
 double camera_angle = (3 * M_PI) / 2;
 double light_angle = 0;
-double camera_angle_speed = 0.05
-double light_angle_speed = 0.0005
-double mas = []
-bool stop_animation = False
+double camera_angle_speed = 0.05;
+double light_angle_speed = 0.0005;
+vector< int > mas;
+bool stop_animation = False;
 
-int matrix_model = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+int matrix_model = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
 
 void init(void) {
   glutInit(&argc,argv);
@@ -84,8 +81,98 @@ void special_keyboard_func( int key, int x1, int y1 ) {
   glutPostRedisplay(); 
 }
 
+/* Функція обробляє повідомлення від клавіатури */
+void keyboard_func( unsigned char key, int x1, int y1 ) {
+#define ESCAPE '\033'
+  switch (key) {
+     case ESCAPE:  exit(0);
+  }
+
+  if ( key == 'w') {
+      my_scale(2, 1, 1);
+  }
+  if ( key == 's') {
+      my_scale(0.5, 1, 1);
+  }
+  if ( key == 'a') {
+      my_rotate(pi/2);
+  }
+  if ( key == 'd') {
+      my_rotate(-pi/2);
+  }
+
+  glutPostRedisplay();
+}
+
+void func(vector< int > mas) {
+  for( int i = 0; i < 10; i++) {
+    for( int j = 0; j < 10; j ++) {
+      mas.insert(mas.end(), i);
+      mas.insert(mas.end(), j);
+      mas.insert(mas.end(), sin(i) + cos(j));
+
+      mas.insert(mas.end(), i + 1);
+      mas.insert(mas.end(), j);
+      mas.insert(mas.end(), sin(i + 1) + cos(j));
+
+      mas.insert(mas.end(), i);
+      mas.insert(mas.end(), j + 1);
+      mas.insert(mas.end(), sin(i) + cos(j + 1));
+
+      mas.insert(mas.end(), i + 1);
+      mas.insert(mas.end(), j + 1);
+      mas.insert(mas.end(), sin(i + 1) + cos(j + 1));
+    }
+  }
+}
+
+void list_of_commands(void) 
+{
+  glNewList(1, GL_COMPILE);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+  glPushMatrix();
+  glColor3ub(255, 255, 255);
+  glutSolidTorus(2, 3, 8, 8);
+
+  glColor3ub(255, 0, 0);
+  glutWireCube(10);
+
+  glColor3ub(0, 0, 0);
+  glutWireTorus(2, 3, 8, 8);
+
+  glTranslatef(15, 0, 0);
+  glColor3ub(255, 0, 0);
+  glutWireCube(10);
+
+  glTranslatef(4, 0, 0);
+  glColor3ub(255, 255, 255);
+  glRotatef(-90.0,0.0,1.0,0.0);
+  glutSolidCone(5, 8, 15, 15); 
+  
+  glColor3ub(0, 0, 0);
+  glutWireCone(5, 8, 15, 15); 
+  glPopMatrix();
+
+  glPushMatrix();
+  glColor3ub(255, 255, 255);
+  glTranslatef(-15, 0, 0);
+  glutSolidSphere(5, 20, 20);
+
+  glColor3ub(255, 0, 0);
+  glutWireCube(10);
+
+  glColor3ub(0, 0, 0);
+  glutWireSphere(5, 20, 20);
+  glPopMatrix();
+
+  glEndList();
+}
+
+
 void display(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
   glCallList(1);
 
   glPushMatrix();
@@ -101,76 +188,55 @@ void display(void) {
   glutSwapBuffers();
 }
 
-/* Функція обробляє повідомлення від клавіатури */
-void keyboard_func( unsigned char key, int x1, int y1 ) {
-#define ESCAPE '\033'
-  switch (key) {
-     case ESCAPE:  exit(0);
-  }
+void set_camera_and_light(void) {
 
-  if ( key == 'm') {
-    z += 5;
-  }
-  if ( key == 'n' ) {
-    z -= 5;
-  }
+}
 
-  glLoadIdentity();
+void idf(void) {
 
-  cout << "x = " << x << endl;
-  cout << "y = " << y << endl;
-  cout << "z = " << z << endl;
-  cout << "lx = " << lx << endl;
-  cout << "ly = " << ly << endl;
-  cout << "lz = " << lz << endl;
+}
 
-  if ( key == 'q') {
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glOrtho(-40.0, 40.0, -40.0, 40.0, 40, 100);
-      glMatrixMode(GL_MODELVIEW);
-  }
-  if ( key == 'w') {
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      gluPerspective(40, 1, 1, 400);
-      glMatrixMode(GL_MODELVIEW);
-  }
-  if ( key == 'e') {
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glFrustum(-40, 40, -40, 40, 30, 200);
-      glMatrixMode(GL_MODELVIEW);
-  }
+void timf() {
 
-  gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0, 1, 0);
-  glutPostRedisplay();
+}
+
+void read_matrix(void) {
+
+}
+
+void my_translate(int x, int y, int z) {
+
+}
+
+void my_rotate(double angle) {
+
 }
 
 /* Головний цикл додатка */
 int main(int argc, char *argv[])
 {
-  glutInit(&argc,argv);
-
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(600, 600);
-  glutInitWindowPosition(0,0);
-  glutCreateWindow("Lab2");
+  init();
   
   glClearColor(0, 0, 0, 1);
-  glShadeModel(GL_SMOOTH);
-  glEnable(GL_DEPTH_TEST);
 
   glutDisplayFunc(display);
+  glutIdleFunc(idf);
+  glutTimerFunc(40, timf, 0);
   glutKeyboardFunc(keyboard_func);
   glutSpecialFunc(special_keyboard_func);
+
+  glShadeModel(GL_SMOOTH);
   glMatrixMode(GL_PROJECTION);
   gluPerspective(40, 1, 1, 400);
+
   glMatrixMode(GL_MODELVIEW);
-  
-  gluLookAt(x, y, z, 0, 0, 0, 0, 1, 0);
+  glLoadIdentity();
+  init_camera();
+  init_light();
+
+  // func(mas);
   list_of_commands();
+
   glutMainLoop();
- 
   return 0;
 }
