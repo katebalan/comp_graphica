98a9789902763aa2f735d03b59a31400e27c6078
Kate Balan <kate.balan@inuits.eu>
2017-11-13 14:20:20 +0200

    rewrite main function

--- 2/main.cpp
+++ 2/main.cpp
@@ -3,57 +3,88 @@
 #include <cmath>
 #include <stdlib.h>
 
+#define _USE_MATH_DEFINES
+
 using namespace std;
 
-double x = 0, y = 0, z = 80;
-double lx = 0, ly = 0, lz = 0;
-double angle_XY = 0, angle_YZ = 0;
+// double x = 0, y = 0, z = 80;
+// double lx = 0, ly = 0, lz = 0;
+// double angle_XY = 0, angle_YZ = 0;
 
-void list_of_commands(void) 
-{
-  glNewList(1, GL_COMPILE);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+int x_camera = 0, y_camera = -20, z_camera = 50;
+double x_light = 0, y_light = 20, z_light = 4.0;
+double camera_angle = (3 * M_PI) / 2;
+double light_angle = 0;
+double camera_angle_speed = 0.05
+double light_angle_speed = 0.0005
+double mas = []
+bool stop_animation = False
 
-  glPushMatrix();
-  glColor3ub(255, 255, 255);
-  glutSolidTorus(2, 3, 8, 8);
+int matrix_model = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 
-  glColor3ub(255, 0, 0);
-  glutWireCube(10);
+void init(void) {
+  glutInit(&argc,argv);
 
-  glColor3ub(0, 0, 0);
-  glutWireTorus(2, 3, 8, 8);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(600, 600);
+  glutInitWindowPosition(0,0);
+  glutCreateWindow("Lab3");
 
-  glTranslatef(15, 0, 0);
-  glColor3ub(255, 0, 0);
-  glutWireCube(10);
-
-  glTranslatef(4, 0, 0);
-  glColor3ub(255, 255, 255);
-  glRotatef(-90.0,0.0,1.0,0.0);
-  glutSolidCone(5, 8, 15, 15); 
-  
-  glColor3ub(0, 0, 0);
-  glutWireCone(5, 8, 15, 15); 
-  glPopMatrix();
-
-  glPushMatrix();
-  glColor3ub(255, 255, 255);
-  glTranslatef(-15, 0, 0);
-  glutSolidSphere(5, 20, 20);
-
-  glColor3ub(255, 0, 0);
-  glutWireCube(10);
-
-  glColor3ub(0, 0, 0);
-  glutWireSphere(5, 20, 20);
-  glPopMatrix();
-
-  glEndList();
+  glEnable(GL_DEPTH_TEST);
 }
 
-void display(void)
-{
+void init_camera(void) {
+  gluLookAt(x_camera, y_camera, z_camera, 0, 0, 0, 0, 1, 0)
+}
+
+void init_light(void) {
+  double ambientLight = [0.0, 0.0, 0.0, 1.0]
+  double diffuseLight = [1.0, 1.0, 1.0, 1.0]
+  double positionLight = [0.0, 0.0, 100.0, 1.0]
+  double specularLight = [1.0, 1.0, 1.0, 1.0]
+  double black = [0.0, 0.0, 0.0, 1.0]
+  glEnable(GL_LIGHTING)
+  // glLightModelfv(GL_LIGHT_MODEL_AMBIENT, black)
+  glEnable(GL_LIGHT0)
+  // glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, True)
+
+  glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight)
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight)
+  glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight)
+  glLightfv(GL_LIGHT0, GL_POSITION, positionLight)
+
+  // gray = [1.0, 1.0, 1.0, 1]
+  // glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray)
+  
+  glEnable(GL_COLOR_MATERIAL)
+  glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specularLight)
+  glMateriali(GL_FRONT_AND_BACK, GL_SHININESS, 128)
+}
+
+void special_keyboard_func( int key, int x1, int y1 ) {
+  if (key == GLUT_KEY_DOWN) {
+    my_translate(0, -5, 0);
+  }
+  if (key == GLUT_KEY_UP) {
+    my_translate(0, 5, 0);
+  }       
+  if (key == GLUT_KEY_LEFT) {
+    my_translate(-5, 0, 0);
+  }
+  if (key == GLUT_KEY_RIGHT) {
+    my_translate(5, 0, 0);
+  }
+
+  if (key == GLUT_KEY_F1) {
+    glutIdleFunc(None);
+    stop_animation = True;
+  }
+
+  glutPostRedisplay(); 
+}
+
+void display(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glCallList(1);
 
@@ -68,54 +99,6 @@ void display(void)
   glPopMatrix();
 
   glutSwapBuffers();
-}
-
-
-void special_keyboard_func( int key, int x1, int y1 ) {
-  if (key == GLUT_KEY_DOWN) {
-    angle_XY += 0.05;
-    if (z > 0) {
-      ly = sin(angle_XY);
-      lz = -cos(angle_XY);
-    }
-    if (z < 0) {
-      ly = sin(angle_XY);
-      lz = cos(angle_XY);
-    }
-  }
-  if (key == GLUT_KEY_UP) {
-    angle_XY -= 0.05;
-    if (z > 0) {
-      ly = sin(angle_XY);
-      lz = -cos(angle_XY);
-    }
-    if (z < 0) {
-      ly = sin(angle_XY);
-      lz = cos(angle_XY);
-    }
-  }       
-  if (key == GLUT_KEY_LEFT) {
-    angle_YZ += 0.05;
-    lx = sin(angle_YZ);
-    lz = -cos(angle_YZ);
-  }
-  if (key == GLUT_KEY_RIGHT) {
-    angle_YZ -= 0.05;
-    lx = sin(angle_YZ);
-    lz = -cos(angle_YZ);
-  }
-
-  glLoadIdentity();
-
-  cout << "x = " << x << endl;
-  cout << "y = " << y << endl;
-  cout << "z = " << z << endl;
-  cout << "lx = " << lx << endl;
-  cout << "ly = " << ly << endl;
-  cout << "lz = " << lz << endl;
-
-  gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0, 1, 0);
-  glutPostRedisplay(); 
 }
 
 /* Функція обробляє повідомлення від клавіатури */
