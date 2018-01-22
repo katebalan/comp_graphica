542992e7c8f4855af9080b4e3610d4275e96e350
Kate <ket11141@gmail.com>
2018-01-22 11:08:30 +0200

    reorganization

--- cg_lab2/main.cpp
+++ cg_lab2/main.cpp
@@ -23,10 +23,7 @@ void init(void) {
 }
 
 /* ця функція керує всім виведенням на екран */
-void Display(void)
-{
- 
-
+void Display(void) {
   glNewList(1, GL_COMPILE);
     glTranslated(4.0, 0.0, 0.0); 
     glColor3f(0.9, 0.3, 0.2); 
@@ -46,57 +43,57 @@ void Display(void)
   gluLookAt(X, Y, Z, XC, YC, ZC, 0, 1, 0);
 
 
-    double vertex[300];
-    double x = 0, y = 0, z = 0, d = 1;
-     for (int i = 0; i<10; i++)
-     { for (int j = 0; j<10; j++)
-      { int k = (i*10 + j)*3;
-       z = sin(x) + cos(y);
-       vertex[k] = x;
-       vertex[k + 1] = y;
-       vertex[k + 2] = z;
-       x += d;
-      }
-      x = 0;
-      y += d;
-     }
-
-   int quads[324];
-   int index = 0;
-   for (int i = 0; i<10; i++)
-   { for (int j = 0; j<9; j++)
-    { int k = i*9 + j;
-     if (k % 10 == 9)
-      continue;
-     quads[index] = k;
-     quads[index + 1] = k + 1;
-     quads[index + 2] = k + 11;
-     quads[index + 3] = k + 10;
-     index += 4;
+  double vertex[300];
+  double x = 0, y = 0, z = 0, d = 1;
+  for (int i = 0; i<10; i++) { 
+    for (int j = 0; j<10; j++) { 
+      int k = (i*10 + j)*3;
+      z = sin(x) + cos(y);
+      vertex[k] = x;
+      vertex[k + 1] = y;
+      vertex[k + 2] = z;
+      x += d;
     }
-   }
-    glTranslatef(-4, -1, 0);
+    x = 0;
+    y += d;
+  }
+
+  int quads[324];
+  int index = 0;
+  for (int i = 0; i<10; i++) { 
+    for (int j = 0; j<9; j++) { 
+      int k = i*9 + j;
+      if (k % 10 == 9)
+      continue;
+      quads[index] = k;
+      quads[index + 1] = k + 1;
+      quads[index + 2] = k + 11;
+      quads[index + 3] = k + 10;
+      index += 4;
+    }
+  }
+
+  glTranslatef(-4, -1, 0);
   glEnableClientState(GL_VERTEX_ARRAY);
-   glVertexPointer(3, GL_DOUBLE, 0, vertex);
+  glVertexPointer(3, GL_DOUBLE, 0, vertex);
 
-   glColor3d(1.0, 1.0, 1.0);
-   glPolygonMode(GL_FRONT, GL_FILL);
-   glDrawElements(GL_QUADS, 324, GL_UNSIGNED_INT, quads);
+  glColor3d(1.0, 1.0, 1.0);
+  glPolygonMode(GL_FRONT, GL_FILL);
+  glDrawElements(GL_QUADS, 324, GL_UNSIGNED_INT, quads);
 
-   glColor3d(0.0, 0.0, 0.0);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glDrawElements(GL_QUADS, 324, GL_UNSIGNED_INT, quads);
-glDisableClientState(GL_VERTEX_ARRAY);
+  glColor3d(0.0, 0.0, 0.0);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glDrawElements(GL_QUADS, 324, GL_UNSIGNED_INT, quads);
+  glDisableClientState(GL_VERTEX_ARRAY);
 
- glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-   glTranslatef(-6,-4,0);
-    glColor3f (0.7, 0, 1);
-   glCallList(1);
-   glCallList(1);
-   glCallList(1);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  glTranslatef(-6,-4,0);
+  glColor3f (0.7, 0, 1);
 
-    
-    // glFlush();
+  glCallList(1);
+  glCallList(1);
+  glCallList(1);
+
   glutSwapBuffers(); 
 }
 
@@ -119,9 +116,7 @@ void Keyboard( unsigned char key, int x, int y ) {
 }
 
 /* Головний цикл додатка */
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
   glutInit(&argc,argv);
 
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
